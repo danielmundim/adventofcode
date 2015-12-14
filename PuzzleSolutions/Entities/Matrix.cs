9ebdbe7f7528c7e013df52b9f4418d6c742943d0
Daniel Mundim <dmundim.cr@gmail.com>
2015-12-14 03:31:53 -0300

    Project structure changes
    Still figuring it out the best way to publish this

---
+++ PuzzleSolutions/Entities/Matrix.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Matrix
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int presents { get; set; }
+    }
+}
